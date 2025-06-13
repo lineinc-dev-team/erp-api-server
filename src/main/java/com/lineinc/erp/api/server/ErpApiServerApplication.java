1bd48e70cae7828d1d558b3f31e9451ab4142773
zwoong-line <cjw6867@lineinc.co.kr>
2025-06-13 10:30:43 +0900

    🎉 프로젝트 초기 셋팅

---
+++ src/main/java/com/lineinc/erp/api/server/ErpApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.lineinc.erp.api.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApiServerApplication.class, args);
+	}
+
+}
