f0c6f4835acf0f03cd593b168ef7694f5b8e800c
zwoong-line <cjw6867@lineinc.co.kr>
2025-07-09 20:41:00 +0900

    ➖ Hot Reload 미사용 모듈로 제거

--- src/main/java/com/lineinc/erp/api/server/presentation/v1/RootController.java
+++ src/main/java/com/lineinc/erp/api/server/presentation/v1/RootController.java
@@ -1,11 +1,20 @@
 package com.lineinc.erp.api.server.presentation.v1;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health")
 public class RootController {
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content()),
+    })
+    @Operation(summary = "ERP API 서버 상태 확인용 루트 엔드포인트")
     @GetMapping("/")
     public String home() {
         return "ERP API Server is running.";
