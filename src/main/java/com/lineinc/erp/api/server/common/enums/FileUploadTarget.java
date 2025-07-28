5cd207d3fbab92adcdeb7f04a10dee78a15d4107
zwoong-line <cjw6867@lineinc.co.kr>
2025-07-28 11:59:34 +0900

    ✨ Presigned URL 요청 타입에 자재관리 추가

--- src/main/java/com/lineinc/erp/api/server/common/enums/FileUploadTarget.java
+++ src/main/java/com/lineinc/erp/api/server/common/enums/FileUploadTarget.java
@@ -7,7 +7,8 @@ public enum FileUploadTarget {
     CLIENT_COMPANY("client-company"),
     SITE("site"),
     MANAGEMENT_COST("management-cost"),
-    STEEL_MANAGEMENT("steel-management");
+    STEEL_MANAGEMENT("steel-management"),
+    MATERIAL_MANAGEMENT("material-management");
 
     private final String directory;
 
