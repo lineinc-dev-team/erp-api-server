4ff0932e241700f4ba23511c52be5cb7e5e4b4e1
zwoong-line <cjw6867@lineinc.co.kr>
2025-07-29 16:35:33 +0900

    🗃️ 권한 현장, 공정 매핑 테이블 추가

---
+++ src/main/java/com/lineinc/erp/api/server/domain/role/entity/RoleSiteProcess.java
@@ -0,0 +1,41 @@
+package com.lineinc.erp.api.server.domain.role.entity;
+
+import com.lineinc.erp.api.server.domain.common.entity.BaseEntity;
+import com.lineinc.erp.api.server.domain.site.entity.Site;
+import com.lineinc.erp.api.server.domain.site.entity.SiteProcess;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@SQLRestriction("deleted = false")
+public class RoleSiteProcess extends BaseEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_site_process_seq")
+    @SequenceGenerator(name = "role_site_process_seq", sequenceName = "role_site_process_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "process_id")
+    private SiteProcess process;
+}
