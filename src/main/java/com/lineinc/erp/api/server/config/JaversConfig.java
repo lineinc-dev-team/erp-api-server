9295000710ec7b99c21d69ca6d8c9c38e3d356dd
zwoong-line <cjw6867@lineinc.co.kr>
2025-07-31 11:57:32 +0900

    ✨ Javers 인메모리 사용해서 객체 비교하게 변경

---
+++ src/main/java/com/lineinc/erp/api/server/config/JaversConfig.java
@@ -0,0 +1,21 @@
+package com.lineinc.erp.api.server.config;
+
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.repository.api.JaversRepository;
+import org.javers.repository.inmemory.InMemoryRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JaversConfig {
+
+    @Bean
+    public Javers javers() {
+        // DB에 저장하지 않고 메모리에서만 비교 기능 사용
+        JaversRepository repository = new InMemoryRepository();
+        return JaversBuilder.javers()
+                .registerJaversRepository(repository)
+                .build();
+    }
+}
