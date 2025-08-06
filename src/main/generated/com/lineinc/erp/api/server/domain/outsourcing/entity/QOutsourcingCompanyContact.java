7007e8d61e9fe415ca62a916c14b3a1e2252fb7b
zwoong-line <cjw6867@lineinc.co.kr>
2025-08-06 10:24:28 +0900

    🗃️ 외주업체 담당자 테이블 추가

---
+++ src/main/generated/com/lineinc/erp/api/server/domain/outsourcing/entity/QOutsourcingCompanyContact.java
@@ -0,0 +1,87 @@
+package com.lineinc.erp.api.server.domain.outsourcing.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QOutsourcingCompanyContact is a Querydsl query type for OutsourcingCompanyContact
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QOutsourcingCompanyContact extends EntityPathBase<OutsourcingCompanyContact> {
+
+    private static final long serialVersionUID = 1445886101L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QOutsourcingCompanyContact outsourcingCompanyContact = new QOutsourcingCompanyContact("outsourcingCompanyContact");
+
+    public final com.lineinc.erp.api.server.domain.common.entity.QBaseEntity _super = new com.lineinc.erp.api.server.domain.common.entity.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.OffsetDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final BooleanPath deleted = _super.deleted;
+
+    //inherited
+    public final DateTimePath<java.time.OffsetDateTime> deletedAt = _super.deletedAt;
+
+    public final StringPath department = createString("department");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isMain = createBoolean("isMain");
+
+    public final StringPath landlineNumber = createString("landlineNumber");
+
+    public final StringPath memo = createString("memo");
+
+    public final StringPath name = createString("name");
+
+    public final QOutsourcingCompany outsourcingCompany;
+
+    public final StringPath phoneNumber = createString("phoneNumber");
+
+    public final StringPath position = createString("position");
+
+    //inherited
+    public final DateTimePath<java.time.OffsetDateTime> updatedAt = _super.updatedAt;
+
+    //inherited
+    public final StringPath updatedBy = _super.updatedBy;
+
+    public QOutsourcingCompanyContact(String variable) {
+        this(OutsourcingCompanyContact.class, forVariable(variable), INITS);
+    }
+
+    public QOutsourcingCompanyContact(Path<? extends OutsourcingCompanyContact> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QOutsourcingCompanyContact(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QOutsourcingCompanyContact(PathMetadata metadata, PathInits inits) {
+        this(OutsourcingCompanyContact.class, metadata, inits);
+    }
+
+    public QOutsourcingCompanyContact(Class<? extends OutsourcingCompanyContact> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.outsourcingCompany = inits.isInitialized("outsourcingCompany") ? new QOutsourcingCompany(forProperty("outsourcingCompany")) : null;
+    }
+
+}
+
---
+++ src/main/java/com/lineinc/erp/api/server/domain/outsourcing/entity/OutsourcingCompanyContact.java
@@ -0,0 +1,68 @@
+package com.lineinc.erp.api.server.domain.outsourcing.entity;
+
+import com.lineinc.erp.api.server.domain.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@SQLRestriction("deleted = false")
+public class OutsourcingCompanyContact extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_company_contact_seq")
+    @SequenceGenerator(name = "client_company_contact_seq", sequenceName = "client_company_contact_seq", allocationSize = 1)
+    private Long id;
+
+    /**
+     * 이 담당자가 속한 발주처 엔티티
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "outsourcing_company_id", nullable = false)
+    private OutsourcingCompany outsourcingCompany;
+
+    /**
+     * 담당자의 이름
+     */
+    @Column(nullable = false)
+    private String name;
+
+    /**
+     * 담당자의 부서
+     */
+    @Column
+    private String department;
+
+    /**
+     * 담당자의 직급
+     */
+    @Column
+    private String position;
+
+    @Column
+    private String landlineNumber;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    /**
+     * 담당자에 대한 비고 또는 추가 메모
+     */
+    @Column(columnDefinition = "TEXT")
+    private String memo;
+
+    /**
+     * 대표 담당자인지 여부
+     */
+    @Column
+    @Builder.Default
+    private Boolean isMain = false;
+}
