4d18048692b2c45f67d6a9a12d5d8a4ba73ecf44
zwoong-line <cjw6867@lineinc.co.kr>
2025-08-27 16:58:37 +0900

    ✨ ERP API 서버의 메인 애플리케이션 클래스 주석 추가

--- src/main/java/com/lineinc/erp/api/server/ErpApiServerApplication.java
+++ src/main/java/com/lineinc/erp/api/server/ErpApiServerApplication.java
@@ -3,9 +3,24 @@ package com.lineinc.erp.api.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ERP API 서버의 메인 애플리케이션 클래스
+ * 
+ * Spring Boot 애플리케이션을 시작하고 자동 설정을 활성화합니다.
+ * 
+ * @SpringBootApplication 어노테이션은 다음을 포함합니다:
+ *                        - @Configuration: 설정 클래스임을 명시
+ *                        - @EnableAutoConfiguration: Spring Boot 자동 설정 활성화
+ *                        - @ComponentScan: 컴포넌트 스캔 활성화
+ */
 @SpringBootApplication
 public class ErpApiServerApplication {
 
+    /**
+     * 애플리케이션의 진입점
+     * 
+     * @param args 명령행 인수
+     */
     public static void main(String[] args) {
         SpringApplication.run(ErpApiServerApplication.class, args);
     }
