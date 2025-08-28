53b4bf132c03d4f6e8e206589b6098d81a57264a
zwoong-line <cjw6867@lineinc.co.kr>
2025-08-28 13:17:35 +0900

    ♻️ 유저 수정시 빈문자 및 null 허용 안됨

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/user/dto/request/UpdateUserRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/user/dto/request/UpdateUserRequest.java
@@ -6,24 +6,27 @@ import com.lineinc.erp.api.server.shared.validation.MultiConstraint;
 import com.lineinc.erp.api.server.shared.validation.ValidatorType;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "유저 수정 요청")
 public record UpdateUserRequest(
-        @Schema(description = "사용자 이름", example = "홍길동") String username,
+        @Schema(description = "사용자 이름", example = "홍길동") @NotBlank String username,
 
-        @Schema(description = "이메일 주소", example = "user@example.com") String email,
+        @Schema(description = "이메일 주소", example = "user@example.com") @NotBlank @Email String email,
 
-        @MultiConstraint(type = ValidatorType.PHONE) @Schema(description = "개인 휴대폰", example = "010-1234-5678") String phoneNumber,
+        @MultiConstraint(type = ValidatorType.PHONE) @Schema(description = "개인 휴대폰", example = "010-1234-5678") @NotBlank String phoneNumber,
 
-        @MultiConstraint(type = ValidatorType.LANDLINE_NUMBER) @Schema(description = "전화번호", example = "02-123-4567") String landlineNumber,
+        @MultiConstraint(type = ValidatorType.LANDLINE_NUMBER) @Schema(description = "전화번호", example = "02-123-4567") @NotBlank String landlineNumber,
 
-        @Schema(description = "사용자 활성화 여부", example = "true") Boolean isActive,
+        @Schema(description = "사용자 활성화 여부", example = "true") @NotNull Boolean isActive,
 
-        @Schema(description = "부서 ID", example = "1") Long departmentId,
+        @Schema(description = "부서 ID", example = "1") @NotNull Long departmentId,
 
-        @Schema(description = "직급 ID", example = "2") Long gradeId,
+        @Schema(description = "직급 ID", example = "2") @NotNull Long gradeId,
 
-        @Schema(description = "직책 ID", example = "3") Long positionId,
+        @Schema(description = "직책 ID", example = "3") @NotNull Long positionId,
 
         @Schema(description = "사용자 메모", example = "외주팀에서 이관됨") String memo,
 
