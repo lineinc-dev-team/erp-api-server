44a8a56e4d3821ff9b48fe811eb264f669636f00
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-01 10:57:06 +0900

    ♻️ 현장 등록시 유효성 검사 로직 추가

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/site/dto/request/CreateSiteFileRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/site/dto/request/CreateSiteFileRequest.java
@@ -2,13 +2,14 @@ package com.lineinc.erp.api.server.interfaces.rest.v1.site.dto.request;
 
 import com.lineinc.erp.api.server.domain.site.enums.SiteFileType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "현장 파일 등록 요청")
 public record CreateSiteFileRequest(
-        @Schema(description = "파일 URL", example = "https://s3.amazonaws.com/bucket/folder/file.pdf") String fileUrl,
+        @NotBlank @Schema(description = "파일 URL", example = "https://s3.amazonaws.com/bucket/folder/file.pdf") String fileUrl,
 
-        @Schema(description = "원본 파일명", example = "file.pdf") String originalFileName,
+        @NotBlank @Schema(description = "원본 파일명", example = "file.pdf") String originalFileName,
 
         @Schema(description = "비고 또는 설명", example = "1차 계약서 스캔본") String memo,
 
