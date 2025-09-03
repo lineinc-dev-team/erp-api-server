33b814e695066d382e6dd7c1b31969061e126def
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-03 09:53:52 +0900

    ✨ 인력정보 상세 반환시 파일 타입 및 타입 코드 반환값 추가

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/labormanagement/dto/response/LaborFileResponse.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/labormanagement/dto/response/LaborFileResponse.java
@@ -3,6 +3,7 @@ package com.lineinc.erp.api.server.interfaces.rest.v1.labormanagement.dto.respon
 import java.time.OffsetDateTime;
 
 import com.lineinc.erp.api.server.domain.labormanagement.entity.LaborFile;
+import com.lineinc.erp.api.server.domain.labormanagement.enums.FileType;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -12,6 +13,8 @@ public record LaborFileResponse(
         @Schema(description = "파일명") String name,
         @Schema(description = "파일 URL") String fileUrl,
         @Schema(description = "원본 파일명") String originalFileName,
+        @Schema(description = "파일 타입") String type,
+        @Schema(description = "파일 타입 코드") FileType typeCode,
         @Schema(description = "메모") String memo,
         @Schema(description = "등록일") OffsetDateTime createdAt,
         @Schema(description = "수정일") OffsetDateTime updatedAt) {
@@ -22,6 +25,8 @@ public record LaborFileResponse(
                 laborFile.getName(),
                 laborFile.getFileUrl(),
                 laborFile.getOriginalFileName(),
+                laborFile.getType() != null ? laborFile.getType().getLabel() : null,
+                laborFile.getType(),
                 laborFile.getMemo(),
                 laborFile.getCreatedAt(),
                 laborFile.getUpdatedAt());
