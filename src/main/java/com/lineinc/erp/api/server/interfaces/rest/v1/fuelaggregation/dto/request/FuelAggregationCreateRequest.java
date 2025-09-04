fd30a1cf913989bee39e1e66fcf35595b63073e7
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-04 16:34:33 +0900

    ♻️ 유류집계 생성 요청 유효성 검사 추가

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/fuelaggregation/dto/request/FuelAggregationCreateRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/fuelaggregation/dto/request/FuelAggregationCreateRequest.java
@@ -6,12 +6,13 @@ import java.util.List;
 import com.lineinc.erp.api.server.domain.fuelaggregation.enums.WeatherType;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "유류집계 생성 요청")
 public record FuelAggregationCreateRequest(
-        @Schema(description = "형장 ID", example = "1") Long siteId,
-        @Schema(description = "공정 ID", example = "1") Long siteProcessId,
-        @Schema(description = "일자", example = "2025-01-15") LocalDate date,
-        @Schema(description = "날씨", example = "SUNNY") WeatherType weather,
+        @Schema(description = "형장 ID", example = "1") @NotNull Long siteId,
+        @Schema(description = "공정 ID", example = "1") @NotNull Long siteProcessId,
+        @Schema(description = "일자", example = "2025-01-15") @NotNull LocalDate date,
+        @Schema(description = "날씨", example = "SUNNY") @NotNull WeatherType weather,
         @Schema(description = "유류정보 목록") List<FuelInfoCreateRequest> fuelInfos) {
 }
