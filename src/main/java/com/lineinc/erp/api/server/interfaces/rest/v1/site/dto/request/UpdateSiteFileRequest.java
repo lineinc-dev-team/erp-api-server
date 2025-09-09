5c8f73a29550f2e10e22a73fd527c3908c876b04
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-09 15:28:57 +0900

    ✨ 현장 파일 수정 요청시 ID, 문서명 null허용

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/site/dto/request/UpdateSiteFileRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/site/dto/request/UpdateSiteFileRequest.java
@@ -2,14 +2,12 @@ package com.lineinc.erp.api.server.interfaces.rest.v1.site.dto.request;
 
 import com.lineinc.erp.api.server.domain.site.enums.SiteFileType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "현장 파일 수정 요청")
 public record UpdateSiteFileRequest(
-        @Schema(description = "현장 파일 ID", example = "1") @NotNull Long id,
+        @Schema(description = "현장 파일 ID", example = "1") Long id,
 
-        @Schema(description = "문서명", example = "현장도면 v1") @NotBlank String name,
+        @Schema(description = "문서명", example = "현장도면 v1") String name,
 
         @Schema(description = "파일 URL", example = "https://s3.amazonaws.com/bucket/folder/file.pdf") String fileUrl,
 
