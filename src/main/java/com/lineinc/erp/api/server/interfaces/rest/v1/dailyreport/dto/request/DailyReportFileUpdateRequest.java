ac9bf50e453083ad4c4447f590a5f8b54df1b5fb
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-09 13:43:14 +0900

    ⚰️ 출역일보 파일 수정 요청

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/dailyreport/dto/request/DailyReportFileUpdateRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/dailyreport/dto/request/DailyReportFileUpdateRequest.java
@@ -3,7 +3,6 @@ package com.lineinc.erp.api.server.interfaces.rest.v1.dailyreport.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
