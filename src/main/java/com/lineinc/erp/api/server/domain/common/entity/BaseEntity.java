cb1d5fcb65fa386899032f9cc46c49d18bfde216
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-10 15:09:47 +0900

    ⚰️ BaseEntity 불필요 import 코드 제거

--- src/main/java/com/lineinc/erp/api/server/domain/common/entity/BaseEntity.java
+++ src/main/java/com/lineinc/erp/api/server/domain/common/entity/BaseEntity.java
@@ -3,7 +3,6 @@ package com.lineinc.erp.api.server.domain.common.entity;
 import java.time.OffsetDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.javers.core.metamodel.annotation.DiffInclude;
 import org.springframework.data.annotation.CreatedBy;
