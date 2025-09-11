9949aeb56fda8036a51d28e8b44772747093015b
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-11 10:44:22 +0900

    ⚰️ LaborType 직접등록 제거

--- src/main/java/com/lineinc/erp/api/server/domain/labormanagement/enums/LaborType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/labormanagement/enums/LaborType.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 public enum LaborType {
     REGULAR_EMPLOYEE("정직원"),
     DIRECT_CONTRACT("직영/계약직"),
-    DIRECT_REGISTRATION("직접등록"),
     ETC("기타");
 
     private final String label;
