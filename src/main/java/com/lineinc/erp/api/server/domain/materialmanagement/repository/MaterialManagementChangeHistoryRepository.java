05f2e4e6ff4513e5843eb58f31f569cee04828df
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-12 15:50:37 +0900

    ⚰️ 미사용 관리비 코드 제거

--- src/main/java/com/lineinc/erp/api/server/domain/materialmanagement/repository/MaterialManagementChangeHistoryRepository.java
+++ src/main/java/com/lineinc/erp/api/server/domain/materialmanagement/repository/MaterialManagementChangeHistoryRepository.java
@@ -5,14 +5,10 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lineinc.erp.api.server.domain.materialmanagement.entity.MaterialManagement;
 import com.lineinc.erp.api.server.domain.materialmanagement.entity.MaterialManagementChangeHistory;
 
 @Repository
 public interface MaterialManagementChangeHistoryRepository
         extends JpaRepository<MaterialManagementChangeHistory, Long> {
-    Slice<MaterialManagementChangeHistory> findByMaterialManagement(MaterialManagement materialManagement,
-            Pageable pageable);
-
     Slice<MaterialManagementChangeHistory> findByMaterialManagementId(Long materialManagementId, Pageable pageable);
 }
