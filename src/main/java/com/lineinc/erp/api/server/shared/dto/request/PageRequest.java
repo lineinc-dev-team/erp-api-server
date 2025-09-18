0aeea4c793114390db1fe6168bcf5dcdd6871d45
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-18 09:58:22 +0900

    ♻️ 공통 페이징 요청 record 객체 코드 개선

--- src/main/java/com/lineinc/erp/api/server/shared/dto/request/PageRequest.java
+++ src/main/java/com/lineinc/erp/api/server/shared/dto/request/PageRequest.java
@@ -1,6 +1,7 @@
 package com.lineinc.erp.api.server.shared.dto.request;
 
 import org.springdoc.core.annotations.ParameterObject;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -8,9 +9,8 @@ import jakarta.validation.constraints.Min;
 @ParameterObject
 @Schema(description = "공통 페이징 요청")
 public record PageRequest(
-        @Min(0) @Schema(description = "0부터 시작하는 페이지 번호", example = "0") Integer page,
-
-        @Min(1) @Max(200) @Schema(description = "한 페이지에 포함될 아이템 수", example = "20") Integer size) {
+        @Schema(description = "0부터 시작하는 페이지 번호", example = "0") @Min(0) Integer page,
+        @Schema(description = "한 페이지에 포함될 아이템 수", example = "20") @Min(1) @Max(200) Integer size) {
     public PageRequest {
         if (page == null)
             page = 0;
