27fe1154c104324088eddb011cd0f22d9aba09f2
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-18 18:47:01 +0900

    ♻️ 직급 목록 조회 API 직급 정렬 기준 추가

--- src/main/java/com/lineinc/erp/api/server/domain/organization/service/GradeService.java
+++ src/main/java/com/lineinc/erp/api/server/domain/organization/service/GradeService.java
@@ -1,15 +1,17 @@
 package com.lineinc.erp.api.server.domain.organization.service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.lineinc.erp.api.server.domain.organization.entity.Grade;
 import com.lineinc.erp.api.server.domain.organization.repository.GradeRepository;
 import com.lineinc.erp.api.server.interfaces.rest.v1.organization.dto.response.GradeResponse;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +21,9 @@ public class GradeService {
 
     @Transactional(readOnly = true)
     public List<GradeResponse> getAllGrades() {
-        List<Grade> grades = gradeRepository.findAll();
+        final List<Grade> grades = gradeRepository.findAll();
         return grades.stream()
+                .sorted(Comparator.comparing(Grade::getId))
                 .map(grade -> new GradeResponse(grade.getId(), grade.getName()))
                 .collect(Collectors.toList());
     }
--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/organization/controller/GradeController.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/organization/controller/GradeController.java
@@ -1,19 +1,21 @@
 package com.lineinc.erp.api.server.interfaces.rest.v1.organization.controller;
 
-import com.lineinc.erp.api.server.domain.organization.service.GradeService;
-import com.lineinc.erp.api.server.interfaces.rest.v1.organization.dto.response.GradeResponse;
-import com.lineinc.erp.api.server.shared.dto.response.SuccessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lineinc.erp.api.server.domain.organization.service.GradeService;
+import com.lineinc.erp.api.server.interfaces.rest.v1.organization.dto.response.GradeResponse;
+import com.lineinc.erp.api.server.shared.dto.response.SuccessResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/grades")
@@ -29,7 +31,7 @@ public class GradeController {
     })
     @GetMapping
     public ResponseEntity<SuccessResponse<List<GradeResponse>>> getAllGrades() {
-        List<GradeResponse> grades = gradeService.getAllGrades();
+        final List<GradeResponse> grades = gradeService.getAllGrades();
         return ResponseEntity.ok(SuccessResponse.of(grades));
     }
 
