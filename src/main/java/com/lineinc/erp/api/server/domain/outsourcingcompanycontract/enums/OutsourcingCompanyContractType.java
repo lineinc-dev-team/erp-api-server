528497f5f3a418cfd4ddf1cc3514f052f0352a0f
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-25 10:29:01 +0900

    ♻️ 외주업체 및 외주업체 계약 유류 재료, 유류, 관리 추가

--- src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractType.java
@@ -9,6 +9,9 @@ public enum OutsourcingCompanyContractType {
     SERVICE("노무"),
     EQUIPMENT("장비"),
     CONSTRUCTION("외주"),
+    MATERIAL("재료"),
+    FUEL("유류"),
+    MANAGEMENT("관리"),
     ETC("기타");
 
     private final String label;
