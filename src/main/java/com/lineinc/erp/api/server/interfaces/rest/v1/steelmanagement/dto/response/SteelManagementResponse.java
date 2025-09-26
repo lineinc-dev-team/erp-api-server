602f839a7b95d4df3d5759055e905527584ea902
zwoong-line <cjw6867@lineinc.co.kr>
2025-09-26 15:49:57 +0900

    ✨ 강재 관리 목록 응답 API에 이전 강재 수불 유형 및 코드 반환값 추가

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/steelmanagement/dto/response/SteelManagementResponse.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/steelmanagement/dto/response/SteelManagementResponse.java
@@ -12,33 +12,21 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "강재 관리 목록 응답")
 public record SteelManagementResponse(
         @Schema(description = "강재 관리 ID", example = "1") Long id,
-
         @Schema(description = "용도", example = "철근 콘크리트 타설용") String usage,
-
         @Schema(description = "강재 수불 유형", example = "PURCHASE") String type,
-
         @Schema(description = "강재 수불 유형 코드", example = "PURCHASE") String typeCode,
-
+        @Schema(description = "이전 강재 수불 유형", example = "PURCHASE") String previousType,
+        @Schema(description = "이전 강재 수불 유형 코드", example = "PURCHASE") String previousTypeCode,
         @Schema(description = "기간 시작일", example = "2024-07-01T00:00:00+09:00") OffsetDateTime startDate,
-
         @Schema(description = "기간 종료일", example = "2024-07-31T00:00:00+09:00") OffsetDateTime endDate,
-
         @Schema(description = "주문일", example = "2024-07-15T00:00:00+09:00") OffsetDateTime orderDate,
-
         @Schema(description = "승인일", example = "2024-07-20T00:00:00+09:00") OffsetDateTime approvalDate,
-
         @Schema(description = "반출일", example = "2024-07-25T00:00:00+09:00") OffsetDateTime releaseDate,
-
         @Schema(description = "첨부파일 존재 여부", example = "true") boolean hasFile,
-
         @Schema(description = "비고", example = "7월 강재 입출고 내역") String memo,
-
         @Schema(description = "현장 요약 정보") SiteResponse.SiteSimpleResponse site,
-
         @Schema(description = "공정 요약 정보") SiteProcessResponse.SiteProcessSimpleResponse process,
-
         @Schema(description = "외주업체 요약 정보") CompanyResponse.CompanySimpleResponse outsourcingCompany,
-
         @Schema(description = "총금액", example = "1000000") Long totalAmount
 
 ) {
@@ -53,6 +41,8 @@ public record SteelManagementResponse(
                 entity.getUsage(),
                 entity.getType() != null ? entity.getType().getLabel() : null,
                 entity.getType() != null ? entity.getType().name() : null,
+                entity.getPreviousType() != null ? entity.getPreviousType().getLabel() : null,
+                entity.getPreviousType() != null ? entity.getPreviousType().name() : null,
                 entity.getStartDate(),
                 entity.getEndDate(),
                 entity.getOrderDate(),
