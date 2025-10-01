01f61c579f7aa95eb1031657e914925c6f00be75
zwoong-line <cjw6867@lineinc.co.kr>
2025-10-01 11:19:54 +0900

    ✨ 삭제된 메뉴는 반환 안되도록 메뉴 API 수정

--- src/main/java/com/lineinc/erp/api/server/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/lineinc/erp/api/server/domain/menu/repository/MenuRepository.java
@@ -13,6 +13,6 @@ import com.lineinc.erp.api.server.domain.menu.entity.Menu;
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     Optional<Menu> findByName(String name);
 
-    @Query("SELECT m FROM Menu m LEFT JOIN FETCH m.permissions p ORDER BY m.order, p.order")
+    @Query("SELECT m FROM Menu m LEFT JOIN FETCH m.permissions p WHERE m.deleted = false ORDER BY m.order, p.order")
     List<Menu> findAllWithPermissions();
 }
