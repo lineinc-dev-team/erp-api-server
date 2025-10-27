dac8db40a19f031d6bf619257ead3d56a791f6c3
zwoong-line <cjw6867@lineinc.co.kr>
2025-10-27 17:05:52 +0900

    ✨ 집계 관련 엑셀 다운로드 target 추가

--- src/main/java/com/lineinc/erp/api/server/shared/enums/FileUploadTarget.java
+++ src/main/java/com/lineinc/erp/api/server/shared/enums/FileUploadTarget.java
@@ -9,12 +9,24 @@ public enum FileUploadTarget {
     OUTSOURCING_COMPANY_CONTRACT("outsourcing-company-contract"),
     SITE("site"),
     MANAGEMENT_COST("management-cost"),
+    SITE_MANAGEMENT_COST("site-management-cost"),
     STEEL_MANAGEMENT("steel-management"),
     MATERIAL_MANAGEMENT("material-management"),
     FUEL_AGGREGATION("fuel-aggregation"),
     LABOR_MANAGEMENT("labor-management"),
     WORK_DAILY_REPORT("work-daily-report"),
-    SITE_MANAGEMENT_COST("site-management-cost");
+
+    // 집계 관련
+    AGGREGATION_TABLE("aggregation-table"),
+    AGGREGATION_MATERIAL_COST("aggregation-material-cost"),
+    AGGREGATION_FUEL("aggregation-fuel"),
+    AGGREGATION_LABOR_COST("aggregation-labor-cost"),
+    AGGREGATION_LABOR_PAYROLL("aggregation-labor-payroll"),
+    AGGREGATION_EQUIPMENT_COST("aggregation-equipment-cost"),
+    AGGREGATION_EQUIPMENT_OPERATION("aggregation-equipment-operation"),
+    AGGREGATION_MANAGEMENT_COST("aggregation-management-cost"),
+    AGGREGATION_ADVANCE_PAYMENT("aggregation-advance-payment"),
+    AGGREGATION_OUTSOURCING_CONSTRUCTION("aggregation-outsourcing-construction");
 
     private final String directory;
 
