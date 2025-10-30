0e37472c28895cfaa910e594318165c7462a3558
zwoong-line <cjw6867@lineinc.co.kr>
2025-10-30 20:02:36 +0900

    ✨ 집계 관리비 조회 API Swagger 명칭 변경

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/ManagementCostAggregationController.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/ManagementCostAggregationController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lineinc.erp.api.server.domain.aggregation.managementcost.service.ManagementCostAggregationService;
+import com.lineinc.erp.api.server.domain.permission.enums.PermissionAction;
+import com.lineinc.erp.api.server.infrastructure.config.security.RequireMenuPermission;
 import com.lineinc.erp.api.server.interfaces.rest.common.BaseController;
 import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.request.ManagementCostAggregationRequest;
 import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.response.ManagementCostAggregationResponse;
+import com.lineinc.erp.api.server.shared.constant.AppConstants;
 import com.lineinc.erp.api.server.shared.dto.response.SuccessResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,10 +28,12 @@ public class ManagementCostAggregationController extends BaseController {
     private final ManagementCostAggregationService managementCostAggregationService;
 
     @GetMapping
-    @Operation(summary = "관리비 집계 조회", description = "현장/공정/조회월 기준 관리비를 업체+항목별로 집계(전회, 금회)하여 반환합니다. 기타 항목은 항목설명 기준.")
+    @Operation(summary = "관리비 조회")
+    @RequireMenuPermission(menu = AppConstants.MENU_AGGREGATION_MANAGEMENT, action = PermissionAction.VIEW)
     public ResponseEntity<SuccessResponse<ManagementCostAggregationResponse>> getManagementCostAggregation(
             @Valid final ManagementCostAggregationRequest request) {
-        final ManagementCostAggregationResponse response = managementCostAggregationService.getManagementCostAggregation(request);
+        final ManagementCostAggregationResponse response = managementCostAggregationService
+                .getManagementCostAggregation(request);
         return SuccessResponse.ok(response);
     }
 }
