4e6d272646fe5034858f38bb56f4dc2da3c1d483
zwoong-line <cjw6867@lineinc.co.kr>
2025-10-30 14:12:13 +0900

    ✨ 노무비 조회 (용역업체별)

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/LaborCostAggregationController.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/LaborCostAggregationController.java
@@ -41,7 +41,7 @@ public class LaborCostAggregationController extends BaseController {
     }
 
     @GetMapping("/outsourcing-labor-cost")
-    @Operation(summary = "용역업체별 노무비 조회")
+    @Operation(summary = "노무비 조회 (용역업체별)")
     public ResponseEntity<SuccessResponse<OutsourcingLaborCostAggregationResponse>> getOutsourcingLaborCostAggregation(
             @Valid final OutsourcingLaborCostAggregationRequest request) {
         final OutsourcingLaborCostAggregationResponse response = outsourcingLaborCostAggregationService
