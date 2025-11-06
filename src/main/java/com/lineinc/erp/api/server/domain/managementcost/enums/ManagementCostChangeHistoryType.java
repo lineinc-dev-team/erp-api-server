9b8492af53679130a0f29accb5d29b2b09bd685a
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-06 10:41:43 +0900

    ✨ 관리비 수정 이력 구분값에 공사 -> 외주 수정

--- src/main/java/com/lineinc/erp/api/server/domain/managementcost/enums/ManagementCostChangeHistoryType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/managementcost/enums/ManagementCostChangeHistoryType.java
@@ -12,7 +12,7 @@ public enum ManagementCostChangeHistoryType {
     MEAL_FEE_DIRECT_CONTRACT("직영"),
     MEAL_FEE_OUTSOURCING("용역"),
     MEAL_FEE_EQUIPMENT("장비"),
-    MEAL_FEE_CONSTRUCTION("공사"),
+    MEAL_FEE_OUTSOURCING_CONTRACT("외주"),
     ITEM_DETAIL("품목상세"),
     KEY_MONEY_DETAIL("전도금 상세"),
     ATTACHMENT("증빙서류");
