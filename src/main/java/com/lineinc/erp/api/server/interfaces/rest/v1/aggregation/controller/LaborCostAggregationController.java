340b94fe283f7fcf9cb551ebbc537bad4cead9c9
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-11 10:03:19 +0900

    ✨ 집계 노무명세서 조회 API 추가

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/LaborCostAggregationController.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/aggregation/controller/LaborCostAggregationController.java
@@ -1,12 +1,16 @@
 package com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lineinc.erp.api.server.domain.aggregation.laborcost.service.LaborCostAggregationService;
 import com.lineinc.erp.api.server.domain.aggregation.outsourcinglaborcost.service.OutsourcingLaborCostAggregationService;
+import com.lineinc.erp.api.server.domain.laborpayroll.service.v1.LaborPayrollService;
 import com.lineinc.erp.api.server.domain.permission.enums.PermissionAction;
 import com.lineinc.erp.api.server.infrastructure.config.security.RequireMenuPermission;
 import com.lineinc.erp.api.server.interfaces.rest.common.BaseController;
@@ -14,10 +18,13 @@ import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.request.Lab
 import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.request.OutsourcingLaborCostAggregationRequest;
 import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.response.LaborCostAggregationResponse;
 import com.lineinc.erp.api.server.interfaces.rest.v1.aggregation.dto.response.OutsourcingLaborCostAggregationResponse;
+import com.lineinc.erp.api.server.interfaces.rest.v1.laborpayroll.dto.request.LaborPayrollDetailSearchRequest;
+import com.lineinc.erp.api.server.interfaces.rest.v1.laborpayroll.dto.response.LaborPayrollDetailResponse;
 import com.lineinc.erp.api.server.shared.constant.AppConstants;
 import com.lineinc.erp.api.server.shared.dto.response.SuccessResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +40,7 @@ public class LaborCostAggregationController extends BaseController {
 
     private final LaborCostAggregationService laborCostAggregationService;
     private final OutsourcingLaborCostAggregationService outsourcingLaborCostAggregationService;
+    private final LaborPayrollService laborPayrollService;
 
     @GetMapping("/labor-cost")
     @Operation(summary = "노무비 조회")
@@ -53,4 +61,19 @@ public class LaborCostAggregationController extends BaseController {
                 .getOutsourcingLaborCostAggregation(request.siteId(), request.siteProcessId(), request.yearMonth());
         return ResponseEntity.ok(SuccessResponse.of(response));
     }
+
+    /**
+     * 노무명세서 상세 조회
+     * 현장, 공정, 년월, 노무인력 타입, 일자로 필터링하여 조회
+     */
+    @Operation(summary = "노무명세서 조회")
+    @GetMapping("/labor-payroll")
+    @RequireMenuPermission(menu = AppConstants.MENU_AGGREGATION_MANAGEMENT, action = PermissionAction.VIEW)
+    public ResponseEntity<SuccessResponse<List<LaborPayrollDetailResponse>>> getLaborPayrollDetails(
+            @Parameter(description = "조회 조건") @ModelAttribute final LaborPayrollDetailSearchRequest request) {
+        final List<LaborPayrollDetailResponse> result = laborPayrollService.getLaborPayrollDetails(
+                request.siteId(), request.processId(), request.yearMonth(), request.type());
+        return ResponseEntity.ok(SuccessResponse.of(result));
+    }
+
 }
