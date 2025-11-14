7be11e0092c102a2c78063196026e9f6a3975796
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-14 13:07:50 +0900

    ✨ 파일 업로드 타켓 집계관련 변수 추가

--- src/main/java/com/lineinc/erp/api/server/shared/enums/FileUploadTarget.java
+++ src/main/java/com/lineinc/erp/api/server/shared/enums/FileUploadTarget.java
@@ -1,36 +1,34 @@
 package com.lineinc.erp.api.server.shared.enums;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum FileUploadTarget {
-    CLIENT_COMPANY("client-company"),
-    OUTSOURCING_COMPANY("outsourcing-company"),
-    OUTSOURCING_COMPANY_CONTRACT("outsourcing-company-contract"),
-    SITE("site"),
-    MANAGEMENT_COST("management-cost"),
-    SITE_MANAGEMENT_COST("site-management-cost"),
-    STEEL_MANAGEMENT("steel-management"),
-    MATERIAL_MANAGEMENT("material-management"),
-    FUEL_AGGREGATION("fuel-aggregation"),
-    LABOR_MANAGEMENT("labor-management"),
-    WORK_DAILY_REPORT("work-daily-report"),
+    CLIENT_COMPANY("client-company", "발주처"),
+    OUTSOURCING_COMPANY("outsourcing-company", "외주업체"),
+    OUTSOURCING_COMPANY_CONTRACT("outsourcing-company-contract", "외주업체 계약"),
+    SITE("site", "현장"),
+    MANAGEMENT_COST("management-cost", "관리비"),
+    SITE_MANAGEMENT_COST("site-management-cost", "현장/본사 관리비"),
+    STEEL_MANAGEMENT("steel-management", "강재수불부"),
+    MATERIAL_MANAGEMENT("material-management", "자재 관리"),
+    FUEL_AGGREGATION("fuel-aggregation", "유류집계"),
+    LABOR_MANAGEMENT("labor-management", "노무 관리"),
+    WORK_DAILY_REPORT("work-daily-report", "출역일보"),
 
     // 집계 관련
-    AGGREGATION_TABLE("aggregation-table"),
-    AGGREGATION_MATERIAL_COST("aggregation-material-cost"),
-    AGGREGATION_FUEL("aggregation-fuel"),
-    AGGREGATION_LABOR_COST("aggregation-labor-cost"),
-    AGGREGATION_LABOR_PAYROLL("aggregation-labor-payroll"),
-    AGGREGATION_EQUIPMENT_COST("aggregation-equipment-cost"),
-    AGGREGATION_EQUIPMENT_OPERATION("aggregation-equipment-operation"),
-    AGGREGATION_MANAGEMENT_COST("aggregation-management-cost"),
-    AGGREGATION_ADVANCE_PAYMENT("aggregation-advance-payment"),
-    AGGREGATION_OUTSOURCING_CONSTRUCTION("aggregation-outsourcing-construction");
+    AGGREGATION_TABLE("aggregation-table", "집계표(본사)"),
+    AGGREGATION_MATERIAL_COST("aggregation-material-cost", "재료비"),
+    AGGREGATION_FUEL("aggregation-fuel", "유류집계"),
+    AGGREGATION_LABOR_COST("aggregation-labor-cost", "노무비"),
+    AGGREGATION_LABOR_PAYROLL("aggregation-labor-payroll", "노무비명세서"),
+    AGGREGATION_EQUIPMENT_COST("aggregation-equipment-cost", "장비비"),
+    AGGREGATION_EQUIPMENT_OPERATION("aggregation-equipment-operation", "장비가동현황"),
+    AGGREGATION_MANAGEMENT_COST("aggregation-management-cost", "관리비"),
+    AGGREGATION_OUTSOURCING_CONSTRUCTION("aggregation-outsourcing-construction", "외주");
 
     private final String directory;
-
-    FileUploadTarget(final String directory) {
-        this.directory = directory;
-    }
+    private final String label;
 }
