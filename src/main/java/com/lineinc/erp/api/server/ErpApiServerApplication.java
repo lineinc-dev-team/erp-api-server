45a7441d6346b128590524990cdef0df06feeeb1
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-20 11:40:44 +0900

    ✨ Vsocde 셋팅 변경

--- src/main/java/com/lineinc/erp/api/server/ErpApiServerApplication.java
+++ src/main/java/com/lineinc/erp/api/server/ErpApiServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * ERP API 서버의 메인 애플리케이션 클래스
- * 
+ *
  * Spring Boot 애플리케이션을 시작하고 자동 설정을 활성화합니다.
- * 
+ *
  * @SpringBootApplication 어노테이션은 다음을 포함합니다:
  * - @Configuration: 설정 클래스임을 명시
  * - @EnableAutoConfiguration: Spring Boot 자동 설정 활성화
@@ -20,7 +20,7 @@ public class ErpApiServerApplication {
 
     /**
      * 애플리케이션의 진입점
-     * 
+     *
      * @param args 명령행 인수
      */
     public static void main(final String[] args) {
