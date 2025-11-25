90f79b1663c8a924b7b73281bef70576bc98a9da
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-25 15:18:41 +0900

    ♻️ 자재관리 투입구분값에 주요자재들 전부 제거

--- src/main/java/com/lineinc/erp/api/server/domain/materialmanagement/enums/MaterialManagementInputType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/materialmanagement/enums/MaterialManagementInputType.java
@@ -7,15 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MaterialManagementInputType {
     DIRECT_INPUT("직접입력", 0),
-    MAJOR_PURCHASE("주요자재(구매)", 1),
-    MAJOR_LEASE("주요자재(임대)", 2),
-    MAJOR_INTERNAL("주요자재(자사)", 3),
-    CIVIL_SUPPORT("부대토목자재", 4),
-    TOOL_MISC("잡자재(공구)", 5),
-    METAL_MISC("잡자재(잡철)", 6),
-    SAFETY("안전(안전관리비)", 7),
-    ENVIRONMENT("환경(환경관리비)", 8),
-    TRANSPORT("운반비", 9);
+    CIVIL_SUPPORT("부대토목자재", 1),
+    TOOL_MISC("잡자재(공구)", 2),
+    METAL_MISC("잡자재(잡철)", 3),
+    SAFETY("안전(안전관리비)", 4),
+    ENVIRONMENT("환경(환경관리비)", 5),
+    TRANSPORT("운반비", 6);
 
     private final String label;
     private final int order;
