8f02a6a1ddb5ec491f1cbad139425920668ae2cc
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-25 13:21:35 +0900

    ✨ 외주업체 담당자 등록 및 수정시 개인 휴대폰, 이메일 필수값 제거

--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/outsourcing/dto/request/OutsourcingCompanyContactCreateRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/outsourcing/dto/request/OutsourcingCompanyContactCreateRequest.java
@@ -11,8 +11,8 @@ public record OutsourcingCompanyContactCreateRequest(
         @Schema(description = "부서", example = "시공팀") @NotBlank String department,
         @Schema(description = "직급", example = "대리") @NotBlank String position,
         @Schema(description = "전화번호", example = "02-123-4567") @NotBlank String landlineNumber,
-        @Schema(description = "개인 휴대폰", example = "010-1234-5678") @NotBlank String phoneNumber,
-        @Schema(description = "이메일", example = "contact@outsourcing.com") @NotBlank @Email String email,
+        @Schema(description = "개인 휴대폰", example = "010-1234-5678") String phoneNumber,
+        @Schema(description = "이메일", example = "contact@outsourcing.com") @Email String email,
         @Schema(description = "비고", example = "안전관리 담당") String memo,
         @Schema(description = "대표 담당자 여부", example = "true") @NotNull Boolean isMain) {
 }
--- src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/outsourcing/dto/request/OutsourcingCompanyContactUpdateRequest.java
+++ src/main/java/com/lineinc/erp/api/server/interfaces/rest/v1/outsourcing/dto/request/OutsourcingCompanyContactUpdateRequest.java
@@ -12,8 +12,8 @@ public record OutsourcingCompanyContactUpdateRequest(
         @Schema(description = "부서", example = "시공팀") @NotBlank String department,
         @Schema(description = "직급", example = "대리") @NotBlank String position,
         @Schema(description = "전화번호", example = "02-123-4567") @NotBlank String landlineNumber,
-        @Schema(description = "개인 휴대폰", example = "010-1234-5678") @NotBlank String phoneNumber,
-        @Schema(description = "이메일", example = "contact@outsourcing.com") @Email @NotBlank String email,
+        @Schema(description = "개인 휴대폰", example = "010-1234-5678") String phoneNumber,
+        @Schema(description = "이메일", example = "contact@outsourcing.com") @Email String email,
         @Schema(description = "비고", example = "안전관리 담당") String memo,
         @Schema(description = "대표 담당자 여부", example = "true") Boolean isMain) {
 }
