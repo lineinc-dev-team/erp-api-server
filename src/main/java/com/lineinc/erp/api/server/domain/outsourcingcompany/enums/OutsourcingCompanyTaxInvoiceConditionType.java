af3451163fb8d12a86109008704eedece52e6a12
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-26 14:21:07 +0900

    ✨ Test

--- src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompany/enums/OutsourcingCompanyTaxInvoiceConditionType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompany/enums/OutsourcingCompanyTaxInvoiceConditionType.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OutsourcingCompanyTaxInvoiceConditionType {
-    MONTH_END("월말일괄"),
-    QUARTERLY("분기"),
-    ON_COMPLETION("완료시");
+    MONTH_END("월말일괄"), QUARTERLY("분기"), ON_COMPLETION("완료시"), NOT_ISSUED("발행안함");
 
     private final String label;
 
