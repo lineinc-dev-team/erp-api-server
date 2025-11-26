0a4082dd51fc55154ad056c3882aca03368273e7
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-26 14:20:31 +0900

    ♻️ Test

--- src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractCategoryType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractCategoryType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OutsourcingCompanyContractCategoryType {
-    MONTHLY("월대"), DAILY("일대"), HALF_DAY("반일대"), NOT_ISSUED("발행안함");
+    MONTHLY("월대"), DAILY("일대"), HALF_DAY("반일대");
 
     private final String label;
 }
