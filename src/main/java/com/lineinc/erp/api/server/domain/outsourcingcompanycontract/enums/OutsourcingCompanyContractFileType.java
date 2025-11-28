e7002cb1558cf5d1a014d2eeb4288dbe93b010cc
zwoong-line <cjw6867@lineinc.co.kr>
2025-11-28 15:29:36 +0900

    ✨ 외주업체 계약 파일 타입 추가

--- src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractFileType.java
+++ src/main/java/com/lineinc/erp/api/server/domain/outsourcingcompanycontract/enums/OutsourcingCompanyContractFileType.java
@@ -8,7 +8,11 @@ import lombok.RequiredArgsConstructor;
 public enum OutsourcingCompanyContractFileType {
     CONTRACT("계약서"),
     GUARANTEE("보증서"),
-    BASIC("기본");
+    BASIC("기본"),
+    BUSINESS_REGISTRATION("사업자등록증"),
+    BANK_ACCOUNT_COPY("통장사본"),
+    EQUIPMENT_REGISTRATION("장비등록증"),
+    VEHICLE_INSURANCE("차량보험증권");
 
     private final String label;
 }
